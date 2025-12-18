58061acde41c93fd0133706ede13f629090d778a
b469 <69499965+b469@users.noreply.github.com>
2025-12-18 13:42:34 -0800

    Add files via upload
    Adding file CSS main.ja

---
+++ Project_3 Math_operation folder/js_folder/main.js
@@ -0,0 +1,56 @@
+
+
+// Anchor to HTML Math oeration
+function addition_function(){ // Defining and naming the addining functon
+    var addition = 2+2;
+    document.getElementById("Math").innerHTML = "2+2 =" + addition;// Print result in HTML
+}
+
+
+// Subtraction Fuction code
+function subtraction_function(){  // Defining and naming the subtraction function
+    var subtraction = 5-3;
+    document.getElementById("take").innerHTML = "5-3 =" + subtraction; // Print result in HTML
+}
+
+//  Multiplication
+function Multiplication_function(){ // Defining and naming thd Multiplication function
+    var Multiplication = 6*8;
+    document.getElementById("Mult").innerHTML = "6x8="+ Multiplication; // Print result in HTML
+}
+
+// Divition
+function divition(){ // Defining and naming the Divition function
+    var divition = 48/6;
+    document.getElementById("div").innerHTML = "48/6="+ divition;  // Print result in HTML
+}
+
+// Multiple Operations
+function more_Math(){ // Defining and naming the Multiple Operation
+    simple_math = (1+2)* 10 / 2 -5;
+    document.getElementById("Demo").innerHTML="1 plus 2, multiplied by 10, divided in half and then subtracted by 5 equal "+ simple_math;
+}// // Print result in HTML
+  
+
+
+// Modulus Operater
+function Modulus_Operator(){ //Defining and naming the Modulus Operatou
+    var Math= 26 % 6;
+    document.getElementById("Mat").innerHTML = "When you divide 26 by 6 you have a remainder of: " + Math; 
+} //// Print result in HTML
+
+// Unary Operation
+function negation(){ // Defining and naming the Unary Operation
+    var x = 10;
+    document.getElementById("Min").innerHTML = x; // Print result in HTML
+}
+
+
+
+//Random Operation
+  window.alert(Math.random() / 100 ); // Window Alert on Page
+                                      // Print result in HTML
+
+
+
+    
